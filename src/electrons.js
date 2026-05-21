71f6465bc18a79e7d136601011c6000bf6d82306
Clemens <clesebnet@gmail.com>
2026-05-21 17:33:17 +0200

    Production Code 1, Driver: Clemens, Observer: Desmond

--- src/electrons.js
+++ src/electrons.js
@@ -1,4 +1,11 @@
 function calculateElectrons(dice) {
+
+    if (dice.length < 4){
+        throw new Error("Input must contain at least 4 dice");
+    }
+    if (dice.length > 6){
+        throw new Error("Input must contain at most 6 dice");
+    }
     return 0;
 }
 
